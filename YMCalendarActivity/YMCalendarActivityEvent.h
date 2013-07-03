ad5c684f50ee64a4d03cf0c8ac89a9070f9b76d9
Yusuke Miyazaki <miyazaki.dev@gmail.com>
2013-07-04 02:19:36 +0900

    Add properties to YMCalendarActivityEvent.

--- YMCalendarActivity/YMCalendarActivityEvent.h
+++ YMCalendarActivity/YMCalendarActivityEvent.h
@@ -10,4 +10,14 @@
 
 @interface YMCalendarActivityEvent : NSObject
 
+@property (nonatomic) NSString *title;
+@property (nonatomic) NSString *location;
+@property (nonatomic) NSString *notes;
+@property (nonatomic) NSURL *URL __OSX_AVAILABLE_STARTING(__MAC_10_8,__IPHONE_5_0);
+
+@property (nonatomic) NSTimeZone *timeZone __OSX_AVAILABLE_STARTING(__MAC_10_8,__IPHONE_5_0);
+
+@property (nonatomic) NSDate *startDate;
+@property (nonatomic) NSDate *endDate;
+
 @end
