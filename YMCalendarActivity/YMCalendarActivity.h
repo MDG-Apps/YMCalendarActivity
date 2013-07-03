6472451d7ba0a05196650856f83fa10818730476
Yusuke Miyazaki <miyazaki.dev@gmail.com>
2013-07-04 02:38:38 +0900

    Update YMCalendarActivity.

--- YMCalendarActivity/YMCalendarActivity.h
+++ YMCalendarActivity/YMCalendarActivity.h
@@ -7,7 +7,10 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <EventKitUI/EventKitUI.h>
 
-@interface YMCalendarActivity : UIActivity
+@interface YMCalendarActivity : UIActivity <EKEventEditViewDelegate> {
+    EKEventEditViewController *viewController;
+}
 
 @end
--- YMCalendarActivity/YMCalendarActivity.m
+++ YMCalendarActivity/YMCalendarActivity.m
@@ -7,7 +7,84 @@
 //
 
 #import "YMCalendarActivity.h"
+#import "YMCalendarActivityEvent.h"
 
 @implementation YMCalendarActivity
+
+- (id)init {
+    self = [super init];
+    
+    return self;
+}
+
+- (NSString *)activityType {
+    return NSStringFromClass([self class]);
+}
+
+- (NSString *)activityTitle {
+    return @"Add to Calendar";
+}
+
+- (UIImage *)activityImage {
+    return nil;
+}
+
+- (BOOL)canPerformWithActivityItems:(NSArray *)activityItems {
+    return YES;
+}
+
+- (void)prepareWithActivityItems:(NSArray *)activityItems {
+    for (YMCalendarActivityEvent *event in activityItems) {
+        if (viewController == nil) {
+            viewController = [[EKEventEditViewController alloc] init];
+            
+            EKEventStore *es = [[EKEventStore alloc] init];
+            [es requestAccessToEntityType:EKEntityTypeEvent
+                               completion:^(BOOL granted, NSError *error) {
+                                   if (granted) {
+                                       EKEvent *e = [EKEvent eventWithEventStore:es];
+                                       e.title = event.title;
+                                       e.location = event.location;
+                                       e.notes = event.notes;
+                                       e.URL = event.URL;
+                                       e.timeZone = event.timeZone;
+                                       e.startDate = event.startDate;
+                                       e.endDate = event.endDate;
+                                       
+                                       viewController.eventStore = es;
+                                       viewController.event = e;
+                                   }
+                                   
+                                   viewController.editViewDelegate = self;
+                               }];
+        }
+    }
+}
+
+- (UIViewController *)activityViewController {
+    return viewController;
+}
+
+- (void)activityDidFinish:(BOOL)completed {
+    [super activityDidFinish:completed];
+}
+
+- (void)eventEditViewController:(EKEventEditViewController *)controller didCompleteWithAction:(EKEventEditViewAction)action {
+    NSError *error = nil;
+    
+    switch (action) {
+        case EKEventEditViewActionSaved:
+            [controller.eventStore saveEvent:controller.event span:EKSpanThisEvent error:&error];
+            break;
+            
+        case EKEventEditViewActionCancelled:
+            break;
+            
+        default:
+            break;
+    }
+    
+    [self activityDidFinish:YES];
+}
 
 @end
