1dd547bc8c62fdc1f58f208480640d123a6d8d4f
Yusuke Miyazaki <miyazaki.dev@gmail.com>
2013-07-04 01:57:34 +0900

    Add empty demo project.

---
+++ YMCalendarActivityDemo/YMCalendarActivityDemo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  YMCalendarActivityDemo
+//
+//  Created by Yusuke Miyazaki on 2013/07/04.
+//  Copyright (c) 2013年 Yusuke Miyazaki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ YMCalendarActivityDemo/YMCalendarActivityDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  YMCalendarActivityDemo
+//
+//  Created by Yusuke Miyazaki on 2013/07/04.
+//  Copyright (c) 2013年 Yusuke Miyazaki. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
